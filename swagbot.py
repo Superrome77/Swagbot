2657ef441cf662352221fa0598c2beaea43d46f5
unknown <stephenoconnell77@gmail.com>
2020-07-13 17:47:47 +0100

    final commit

--- swagbot.py
+++ swagbot.py
@@ -3,11 +3,12 @@ import time
 import random
 import selenium
 import os
+import pyautogui
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import pickle
-from FacebookBot import SeleniumTest as logon
 
 class SeleniumTest():
     def __init__(self):
@@ -29,25 +30,33 @@ class SeleniumTest():
         #self._driver.set_window_size(1550,2000)
         self._driver.get("https://www.swagbucks.com/")
         try:
-            with open("cookies.txt", 'rb') as cookiesfile:
+            with open("Swagcookies.txt", 'rb') as cookiesfile:
                 cookies = pickle.load(cookiesfile)
                 for cookie in cookies:
+                    if 'expiry' in cookie:
+                        del cookie['expiry']
+                    print("cookie added")
                     self._driver.add_cookie(cookie)
             self._driver.get("https://www.swagbucks.com/")
-        except:
+        except Exception as e: 
+            print(e)   
+            print("Please sign in")
             time.sleep(120)
-            with open("cookies.txt", 'wb') as filehandler:
+            with open("Swagcookies.txt", 'wb') as filehandler:
                 pickle.dump(self._driver.get_cookies(), filehandler)
     def loadSurvey(self):
         self.print("------------------Load survey start-----------------------")
-        for i in range(10):
+        for i in range(1):
             try:
                 self._driver.get("https://www.swagbucks.com/surveys")
                 surveys = self._driver.find_elements_by_xpath("//a[@class='surveyClick markClicked']")
-                surveys.click[i]
+                print(surveys)
+                if len(surveys) == 1:
+                    return None
+                surveys[i].click()
                 time.sleep(10)
                 self.print("Survey found")
-                execute()
+                self.execute()
             except:
                 self.print("No survey found")
                 pass
@@ -97,17 +106,36 @@ class SeleniumTest():
                 except:
                     pass
             self.print("Playlist selected")
-            time.sleep(3)
-            for i in range(50):
-                try:
+            time.sleep(10)
+            i = 1
+            try:
+                times = self._driver.find_elements_by_xpath("//p[@class='sbWatchPlaylistVideoTime']")
+                for video in times:
+                    duration = video.text
+                    xx = duration.split(":")
+                    if len(xx) == 1:
+                        time.sleep(10)
+                        print("broken")
+                        return None
+                    if len(xx) == 2:
+                        minutes = int(xx[0])
+                        seconds = int(xx[1])
+                        total = (minutes*60 + seconds)
+                    else:
+                        seconds = int(xx[1])
+                        total = (seconds)
+                    print("total %i" %(total))
+                    time.sleep(total)
+                    time.sleep(random.randint(1,5))
                     nextVideo = self._driver.find_element_by_xpath("//a[@index='%i']" %(i))
                     nextVideo.click()
-                    self.print("video playing")
-                    time.sleep(80)
-                except Exception as e:
-                    print("end of playlist")
-                    pass
-            time.sleep(2)
+                    i += 1
+                #time.sleep(90)
+            except Exception as e:
+                print(e)
+                print("end of playlist")
+                pass
+        time.sleep(2)
 
 
     def daily(self):
@@ -140,7 +168,7 @@ class SeleniumTest():
                 dropAnswer = self._driver.find_element_by_xpath("//span[@data-index='%i']" %(dropAnswerRand))
                 dropAnswer.click()
                 time.sleep(1)
-                self._actions.send_keys(Keys.TAB)
+                pyautogui.press("tab")
                 self._actions.send_keys(Keys.ENTER)
                 self._actions.perform()
                 self._actions.reset_actions()
@@ -150,8 +178,6 @@ class SeleniumTest():
             except:
                 count += 1
                 pass
-
-
 
     def answer(self):
         time.sleep(1)
@@ -210,7 +236,7 @@ class SeleniumTest():
             self.surveySubmit()
 
     def surveySubmit(self):
-        submitWait = random.randint(10,30)
+        submitWait = random.randint(1,2)
         time.sleep(submitWait)
         try:
             surveySubmit = self._driver.find_elements_by_xpath("//input[@type='submit']")
@@ -237,18 +263,12 @@ class SeleniumTest():
             self._actions.perform()
             self._actions.reset_actions()
         self._actions.send_keys(Keys.ENTER)
-        self._actions.send_keys(Keys.TAB)
-        self._actions.send_keys(Keys.ENTER)
-        self._actions.perform()
-        self._actions.reset_actions()
         time.sleep(2)
         self._actions.send_keys(Keys.DOWN)
         self._actions.send_keys(Keys.ENTER)
-        self._actions.click()
         self._actions.perform()
         self._actions.reset_actions()
-        time.sleep(2)
-        self.print("Flash enabled")
+        time.sleep(12)
 
     def print(self,string):
         print(string)
@@ -259,6 +279,10 @@ class SeleniumTest():
         balance = self._driver.find_element_by_xpath("//span[@id='sbBalance']")
         self.print("Balance is : %s" %(balance.text))
         self.print("__________________________________END______________________________________________")
+        self._driver.close()
+        pyautogui.keyDown('ctrl')
+        pyautogui.press("c")
+        pyautogui.keyUp('ctrl')
 
     def banner(self):
         try:
@@ -348,26 +372,72 @@ class SeleniumTest():
         reward.click()
         claim = self._driver.find_element_by_xpath("//button[@class='enter-promo-btn promo-code-js']")
         claim.click()
+
+    def dailyDeal(self):
+        try:
+            self._driver.get("https://www.swagbucks.com/")
+            time.sleep(2)
+            self._driver.execute_script("window.scrollBy(0,100)")
+            deal = self._driver.find_element_by_xpath("//*[contains(text(), 'Deal of the Day')]")
+            deal.click()
+            time.sleep(4)
+        except:
+            self.print("----------------------- Daily Deal not interactable ------------------------------")
+            pass
+        
+    def search(self):
+        terms = ["news","local news","weather","local weather","games","youtube","facebook","twitter","memes","twitter news","funny memes","funny","instagram","youtube trending","cats","games news","tumblr",
+                "wikiHow","google","swagbucks","music","spotify","email","gamil","gmail","nwes","wether","utube","twiter","faecbook","stema","steam","redit","reddit","reddit old","reddit login","youtube login",
+                "facebook login","twitter login","gmail login","netlfix","netflix","netflix login","twitch","twitch login","twithc","discrod","discord","discord login","amazon","ebay","yahoo","craiglist","maps",
+                "google maps","google docs","google translate","cnn","hotmail","calculator","google drive","paypal","paypal login","zillow","entertainment","pintrest","speed test","roblox","linkedIn","amazon prime"
+                "sports","hulu","pandora","etsy","nfl","just eat","deliveroo","imdb","expedia","123movies","outlook","gamestop","solitare","ikea","timer","date","art","deviantart","thesaurus","periodic table",
+                "face","map","happy wheels","slitherio","calender","discover","movies","bitcoin","ip","audible","food","cars","google earth","amazon book","skype","firefox","online games","wikipedia","videos",
+                "currency converter","webmd","shoes","miniclip","breaking news","dictionary","amazon uk","joji","post malone","top charts",
+                
+                "news","local nws","weathr","local eather","games","youtube","facebook","twitter","memes","twittr news","funy memes","fnny","instagram","youtube trening","cas","gmes news","tublr",
+                "wikiow","gogle","swagbcks","music","spotiy","email","gamil","gail","nwes","wether","utube","twter","faecbok","stema","stam","redit","reddit","reddi old","reddit ogin","yoube login",
+                "faceook login","twiter login","gmail logn","netlfix","netflix","netflix login","twitc","twich ogin","twihc","discrod","disord","discord login","amzon","ebay","ahoo","crailist","mps",
+                "google maps","gogle dos","google tranlat","cnn","hotmail","calculator","google drive","paypl","paypal loin","zillow","entetainment","pintrest","sped test","robox","linkdIn","amazn prime"
+                "sports","hulu","pandra","etsy","nfl","jut eat","deliveroo","imdb","expedia","123movies","otlook","gametop","solitare","ike","tier","date","art","eviantart","thsaurus","priodic tale",
+                "face","ap","happywheels","sliherio","caender","discover","movies","bitcon","ip","audible","fod","cars","google earth","amazn book","skye","fiefox","online game","wikpedia","vidos",
+                "currncy conveter","webd","shos","minilip","breaking news","dictionary","amazon uk","joji","pot malne","top charts"]
+        while True:
+            self._driver.get("https://search.swagbucks.com/")
+            search = self._driver.find_element_by_xpath("//input[@type='text']")
+            search.send_keys(terms[random.randint(0,len(terms)-1)])
+            time.sleep(random.randint(5,8))
+            enter = self._driver.find_element_by_xpath("//button[@id='sawSubmit']")
+            enter.click()
+            try:
+                time.sleep(5)
+                self._driver.find_element_by_xpath("//img[@id='captchaImg']")
+                break
+            except Exception as e:
+                pass
+            time.sleep(random.randint(60,90))
+            print(self._driver.title)
+        while True:
+            time.sleep(15)
+            if self._driver.title != "cp-claim-prize":
+                pass
+            else:
+                break
+
 if __name__ == "__main__":
     rounds = 0
     t = SeleniumTest()
-    """
     t.banner()
     t.flash()
-    t.game()
+    t.daily()
+    t.profileSurvey()
     t.daily()
     t.game()
     t.profileSurvey()
     t.game()
-    #t.watch("https://www.swagbucks.com/watch/playlists/333/news-politics?sort=8")
-    #t.game()
-    """
-    t.watch3()
-    """
     t.watch("https://www.swagbucks.com/watch/playlists/3/food?sort=8")
     for i in range(30):
         t.game()
         time.sleep(60)
     t.balance()
     t.log()
-    """
+    sys.exit()
